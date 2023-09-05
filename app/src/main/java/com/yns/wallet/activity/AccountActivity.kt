bb0a8c39f09b2948f88d0af827c2669eea034eb2
jj <12345_258@163.com>
2023-09-05 21:40:39 +0800

    切换账号页面弹窗

--- app/src/main/java/com/yns/wallet/activity/AccountActivity.kt
+++ app/src/main/java/com/yns/wallet/activity/AccountActivity.kt
@@ -1,10 +1,13 @@
 package com.yns.wallet.activity
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.yns.wallet.R
@@ -16,7 +19,23 @@ class AccountActivity : BaseActivity<AccountActivityBinding>() {
     override fun initView(root: View, savedInstanceState: Bundle?) {
         viewBinding.rvList.layoutManager = LinearLayoutManager(this)
         viewBinding.rvList.adapter = AccountListAdapter(this)
+
+        viewBinding.tvCreate.setOnClickListener { showEditDialog() }
     }
+
+    private fun showEditDialog() {
+        val view = LayoutInflater.from(this).inflate(R.layout.dialog_edit, null)
+        val dialog = AlertDialog.Builder(this).create()
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.setView(view)
+        dialog.show()
+        val width = resources.getDimensionPixelOffset(R.dimen.dialog_width)
+        val height = dialog.window?.attributes?.height as Int
+        dialog.window?.setLayout(width, height)
+        view.findViewById<View>(R.id.tv_cancel).setOnClickListener { dialog.dismiss() }
+        view.findViewById<View>(R.id.tv_confirm).setOnClickListener { dialog.dismiss() }
+    }
+
 
     class AccountListAdapter(private val context: Context) :
         RecyclerView.Adapter<BottomSheetViewHolder>() {
