9f089ca86ff0bef1dcd1fc73cc431b1adf5a03be
jj <12345_258@163.com>
2023-09-05 21:48:52 +0800

    测试

--- app/src/main/java/com/yns/wallet/AboutUsActivity.kt
+++ app/src/main/java/com/yns/wallet/AboutUsActivity.kt
@@ -3,7 +3,7 @@ package com.yns.wallet
 import android.os.Bundle
 
 class AboutUsActivity :BaseActivity() {
-
+    //
     override fun init(savedInstanceState: Bundle?) {
         setContentView(R.layout.activity_about_us)
     }
