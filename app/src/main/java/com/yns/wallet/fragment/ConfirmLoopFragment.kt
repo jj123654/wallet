843067cd217f9bbc0904e41e65eccee55051edfc
ji123654 <12345_258@163.com>
2023-09-12 21:44:11 +0800

    确认loop 弹出框

--- app/src/main/java/com/yns/wallet/fragment/ConfirmLoopFragment.kt
+++ app/src/main/java/com/yns/wallet/fragment/ConfirmLoopFragment.kt
@@ -1,12 +1,38 @@
 package com.yns.wallet.fragment
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.View
+import android.widget.PopupWindow
+import com.luck.lib.camerax.utils.DensityUtil
 import com.yns.wallet.base.BaseFragment
 import com.yns.wallet.databinding.FragmentConfirmLoopBinding
+import com.yns.wallet.databinding.PopupWindowBalanceBinding
+import com.yns.wallet.util.onClick
 
 class ConfirmLoopFragment : BaseFragment<FragmentConfirmLoopBinding>() {
     override val isDialog: Boolean = true
     override fun initView(root: View, savedInstanceState: Bundle?) {
+        viewBinding.walletName.onClick {
+            showPopup()
+        }
+    }
+
+    var popup: PopupWindow? = null
+    var popupViewBinding: PopupWindowBalanceBinding? = null
+    private fun showPopup() {
+        val p = popup ?: PopupWindow(requireContext()).let {
+            val view = PopupWindowBalanceBinding.inflate(layoutInflater)
+            it.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            it.width = DensityUtil.dip2px(requireContext(), 260f)
+            it.isOutsideTouchable = true
+            it.isTouchable = true
+            popupViewBinding = view
+            it.isFocusable = false
+            it.contentView = view.root
+            it
+        }
+        p.showAsDropDown(viewBinding.walletName)
     }
 }
