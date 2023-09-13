12bf41664d87d55c49c0b37db1783e5f5b6e73ed
jj12365 <12345_258@163.com>
2023-09-13 15:19:27 +0800

    复制按钮

--- app/src/main/java/com/yns/wallet/activity/WalletDetailActivity.kt
+++ app/src/main/java/com/yns/wallet/activity/WalletDetailActivity.kt
@@ -5,11 +5,16 @@ import android.view.View
 import com.yns.wallet.base.BaseActivity
 import com.yns.wallet.databinding.ActivityAboutUsBinding
 import com.yns.wallet.databinding.ActivityDetail2Binding
+import com.yns.wallet.util.copyToClipboard
+import com.yns.wallet.util.onClick
 
 class WalletDetailActivity : BaseActivity<ActivityDetail2Binding>() {
 
     override fun initView(root: View, savedInstanceState: Bundle?) {
-
+        viewBinding.ivMineHash.onClick {
+            val text = viewBinding.ivMineHash.text.toString()
+            text.copyToClipboard(this)
+        }
     }
 
 
