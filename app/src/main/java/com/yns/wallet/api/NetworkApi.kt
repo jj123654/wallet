ef2f5ebe2bef72355fa5a4007b5421e6820f9be6
jj12365 <12345_258@163.com>
2023-09-19 15:49:09 +0800

    工具类

---
+++ app/src/main/java/com/yns/wallet/api/NetworkApi.kt
@@ -0,0 +1,124 @@
+package com.yns.wallet.api
+
+import android.text.TextUtils
+import com.yns.wallet.bean.Response
+import com.yns.wallet.service.Request
+import com.yns.wallet.service.RequestHelper
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.internal.closeQuietly
+import java.lang.StringBuilder
+
+/**
+ *  //时间 默认穿0
+//1.https://apilist.tronscanapi.com/api/tokens/overview?filter=TRX
+//2. https://apilist.tronscanapi.com/api/new/transfer?sort=-timestamp&start=页数&limit=50&address=地址
+//3.https://apilist.tronscanapi.com/api/token_trc20/transfers?limit=50&start=0&contract_address=地址&start_timestamp=时间&relatedAddress=地址
+//4.https://apilist.tronscanapi.com/api/transaction?sort=-timestamp&limit=50&address=地址&start_timestamp=时间
+//5.https://apilist.tronscanapi.com/api/account/tokens?address=地址&start=0&limit=20&hidden=0&show=0&sortType=0&sortBy=0&token=
+
+ */
+object NetworkApi {
+    const val BASE_URL = "https://apilist.tronscanapi.com/api/"
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
+
+    /**
+     * //1.https://apilist.tronscanapi.com/api/tokens/overview?filter=TRX
+
+     */
+    @JvmStatic
+    fun tokenOverView(filter: String? = "TRX", callback: (Response<String>) -> Unit) {
+        scope.launch {
+            val r = getSuspend("tokens/overview", mapOf("filter" to filter))
+            callback.invoke(r)
+        }
+    }
+
+
+    /**
+     * //2. https://apilist.tronscanapi.com/api/new/transfer?sort=-timestamp&start=页数&limit=50&address=地址
+     */
+    @JvmStatic
+    fun transfer(
+        sort: String? = "-timestamp",
+        start: Int = 1,
+        limit: Int = 50,
+        address: String = "",
+        callback: (Response<String>) -> Unit
+    ) {
+        scope.launch {
+            val r = getSuspend(
+                "new/transfer",
+                mapOf("sort" to sort, "start" to start, "limit" to limit, "address" to address)
+            )
+            callback.invoke(r)
+        }
+    }
+
+    /**
+    //3.https://apilist.tronscanapi.com/api/token_trc20/transfers?limit=50&start=0&contract_address=地址&start_timestamp=时间&relatedAddress=地址
+     */
+    @JvmStatic
+    fun tokenTRC20Transfer(
+        start: Int = 1,
+        limit: Int = 50,
+        contract_address: String = "",
+        relatedAddress: String = "",
+        start_timestamp: Long = 0,
+        callback: (Response<String>) -> Unit
+    ) {
+        scope.launch {
+            val r = getSuspend(
+                "token_trc20/transfers",
+                mapOf(
+                    "start" to start,
+                    "limit" to limit,
+                    "contract_address" to contract_address,
+                    "relatedAddress" to relatedAddress,
+                    "start_timestamp" to start_timestamp
+                )
+            )
+            callback.invoke(r)
+        }
+    }
+
+    private suspend fun getSuspend(path: String, params: Map<String, Any?>): Response<String> {
+        return withContext(Dispatchers.IO) {
+            val client = Request.okhttp
+            val url = RequestHelper.check(BASE_URL + path)
+            val r = client.newCall(
+                okhttp3.Request.Builder().url(makeUrl(url, params)).method("GET", null).build()
+            )
+                .execute()
+            try {
+                if (r.isSuccessful) {
+                    val body = r.body?.toString()
+                    if (!TextUtils.isEmpty(body)) {
+                        return@withContext Response.success(body)
+                    }
+                }
+                return@withContext Response.error(r.code, msg = r.body?.string())
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Response.error(-1, msg = e.message)
+            } finally {
+                r.closeQuietly()
+            }
+
+        }
+    }
+
+    private fun makeUrl(url: String, map: Map<String, Any?>): String {
+        val u = if (!url.contains("?")) url + "?" else url
+        val p = StringBuilder()
+        map.forEach {
+            p.append(it.key)
+            p.append("=")
+            p.append(it.value?.toString())
+        }
+        return u + p.toString()
+    }
+}
