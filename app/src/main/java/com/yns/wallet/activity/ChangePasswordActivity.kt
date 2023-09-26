9f0ae38484a311a929d0c74fb5b0d1580f950b5b
jj12365 <12345_258@163.com>
2023-09-26 15:15:31 +0800

    修改密码样式修改

--- app/src/main/java/com/yns/wallet/activity/ChangePasswordActivity.kt
+++ app/src/main/java/com/yns/wallet/activity/ChangePasswordActivity.kt
@@ -2,6 +2,8 @@ package com.yns.wallet.activity
 
 import android.os.Bundle
 import android.view.View
+import com.qmuiteam.qmui.kotlin.onClick
+import com.yns.wallet.R
 import com.yns.wallet.base.BaseActivity
 import com.yns.wallet.databinding.ActivityAboutUsBinding
 import com.yns.wallet.databinding.ActivityChangePwdBinding
@@ -9,7 +11,12 @@ import com.yns.wallet.databinding.ActivityChangePwdBinding
 class ChangePasswordActivity : BaseActivity<ActivityChangePwdBinding>() {
 
     override fun initView(root: View, savedInstanceState: Bundle?) {
+        viewBinding.apply {
+            tvConfirm.onClick {
 
+
+            }
+        }
     }
 
 
