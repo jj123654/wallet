8cb8426c8610d58e76d72cb8d50eedaf46e00983
jj12365 <12345_258@163.com>
2023-11-07 17:00:30 +0800

    去掉webview，保留输入框

--- app/src/main/java/com/yns/wallet/fragment/DiscoverFragment.kt
+++ app/src/main/java/com/yns/wallet/fragment/DiscoverFragment.kt
@@ -30,62 +30,62 @@ class DiscoverFragment:BaseFragment<FragmentDiscoverBinding>() {
                 if(viewBinding.searchEt.text.isNullOrEmpty()){
                     return@setOnEditorActionListener true
                 }
-                viewBinding.webView.loadUrl(viewBinding.searchEt.text.toString())
+//                viewBinding.webView.loadUrl(viewBinding.searchEt.text.toString())
                 return@setOnEditorActionListener true
             }
             return@setOnEditorActionListener false
         }
 
 
-        val webSettings: WebSettings = viewBinding.webView.settings
-        webSettings.useWideViewPort = true
-        webSettings.loadWithOverviewMode = true
-        webSettings.setSupportZoom(true)
-        webSettings.javaScriptEnabled = true
-
-        //启用地理定位，默认为true
-
-        //启用地理定位，默认为true
-        webSettings.setGeolocationEnabled(true)
-        webSettings.databaseEnabled = true
-
-        webSettings.domStorageEnabled = true //2018/6/15 打开本地缓存提供JS调用,
-
-
-        webSettings.setAppCacheMaxSize((1024 * 1024 * 8).toLong()) // 实现8倍缓存
-
-        val appCachePath = context?.cacheDir?.absolutePath
-        webSettings.setAppCachePath(appCachePath)
-        webSettings.allowFileAccess = true
-        webSettings.setAppCacheEnabled(true)
-
-        webSettings.pluginState = WebSettings.PluginState.ON
-        webSettings.cacheMode = WebSettings.LOAD_NO_CACHE
-
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-//        }
-
-        // 设置字符集编码
-
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-//        }
-
-        // 设置字符集编码
-        webSettings.defaultTextEncodingName = "UTF-8"
-        // 开启JavaScript支持
-        // 传递一个Java对象，同时给他命名，这个对象可以在js中调用这个对象的方法
-//        product_webView.setWebChromeClient(new WebChromeClient());
-        //        product_webView.setWebChromeClient(new WebChromeClient());
-        viewBinding.webView.isHardwareAccelerated
-
-//        viewBinding.titleBar.setLeftListener{
-//            onBackPressed()
-//        }
+//        val webSettings: WebSettings = viewBinding.webView.settings
+//        webSettings.useWideViewPort = true
+//        webSettings.loadWithOverviewMode = true
+//        webSettings.setSupportZoom(true)
+//        webSettings.javaScriptEnabled = true
 //
-//        val url = intent.getStringExtra("url")
-        viewBinding.webView.loadUrl("https://www.google.co.uk/")
+//        //启用地理定位，默认为true
+//
+//        //启用地理定位，默认为true
+//        webSettings.setGeolocationEnabled(true)
+//        webSettings.databaseEnabled = true
+//
+//        webSettings.domStorageEnabled = true //2018/6/15 打开本地缓存提供JS调用,
+//
+//
+//        webSettings.setAppCacheMaxSize((1024 * 1024 * 8).toLong()) // 实现8倍缓存
+//
+//        val appCachePath = context?.cacheDir?.absolutePath
+//        webSettings.setAppCachePath(appCachePath)
+//        webSettings.allowFileAccess = true
+//        webSettings.setAppCacheEnabled(true)
+//
+//        webSettings.pluginState = WebSettings.PluginState.ON
+//        webSettings.cacheMode = WebSettings.LOAD_NO_CACHE
+//
+////        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+////            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+////        }
+//
+//        // 设置字符集编码
+//
+////        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+////            webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+////        }
+//
+//        // 设置字符集编码
+//        webSettings.defaultTextEncodingName = "UTF-8"
+//        // 开启JavaScript支持
+//        // 传递一个Java对象，同时给他命名，这个对象可以在js中调用这个对象的方法
+////        product_webView.setWebChromeClient(new WebChromeClient());
+//        //        product_webView.setWebChromeClient(new WebChromeClient());
+//        viewBinding.webView.isHardwareAccelerated
+//
+////        viewBinding.titleBar.setLeftListener{
+////            onBackPressed()
+////        }
+////
+////        val url = intent.getStringExtra("url")
+//        viewBinding.webView.loadUrl("https://www.google.co.uk/")
     }
 
 
